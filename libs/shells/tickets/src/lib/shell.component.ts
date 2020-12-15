167dc00e6ba845a741adf0a6ccdb0daaf9c88401
Kyle Banks <Kyle.Ryan.Banks@gmail.com>
2020-12-15 17:57:57 -0600

    scaffold component shell

---
+++ libs/shells/tickets/src/lib/shell.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nrwl-challenge-shell',
+  template: `
+    <p>
+      shell works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ShellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/shells/tickets/src/lib/shells-tickets.module.ts
+++ libs/shells/tickets/src/lib/shells-tickets.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { ShellComponent } from './shell.component';
 
 export const shellsTicketsRoutes: Route[] = [];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
+  declarations: [ShellComponent],
+  exports: [ShellComponent],
 })
 export class ShellsTicketsModule {}
