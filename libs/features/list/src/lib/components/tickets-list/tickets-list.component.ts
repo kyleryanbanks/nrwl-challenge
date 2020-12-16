48f6d79365e3541650cf17e81f95a2e11798e2ba
Kyle Banks <Kyle.Ryan.Banks@gmail.com>
2020-12-15 18:00:17 -0600

    scaffold tickets list

---
+++ libs/features/list/src/lib/components/tickets-list/tickets-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nrwl-challenge-tickets-list',
+  template: `
+    <p>
+      tickets-list works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TicketsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/features/list/src/lib/features-list.module.ts
+++ libs/features/list/src/lib/features-list.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { TicketsListComponent } from './components/tickets-list/tickets-list.component';
 
 export const featuresListRoutes: Route[] = [];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
+  declarations: [TicketsListComponent],
 })
 export class FeaturesListModule {}
