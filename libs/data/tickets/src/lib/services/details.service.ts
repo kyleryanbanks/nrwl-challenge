5d930cb34c50448773342414c56f635be49f784b
Kyle Banks <Kyle.Ryan.Banks@gmail.com>
2020-12-20 02:16:17 -0600

    Add temporary storage for selected details.

---
+++ libs/data/tickets/src/lib/services/details.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { Ticket } from '../tickets.models';
+
+@Injectable({ providedIn: 'root' })
+export class DetailsService {
+  _ticket: Ticket;
+
+  save(ticket: Ticket) {
+    this._ticket = ticket;
+  }
+
+  load(): Observable<Ticket> {
+    return of(this._ticket);
+  }
+}
--- libs/features/details/src/lib/details.component.ts
+++ libs/features/details/src/lib/details.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  BackendService,
+  DetailsService,
+  Ticket,
+} from '@nrwl-challenge/data/tickets';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'nrwl-challenge-details',
-  template: ` <p>details works!</p> `,
+  template: `
+    <ng-container *ngIf="selectedTicket | async as ticket; else loading">
+      <h1>{{ ticket.description }}</h1>
+    </ng-container>
+    <ng-template #loading>
+      <p>Loading ticket: {{ id }}</p>
+    </ng-template>
+  `,
   styles: [],
 })
-export class DetailsComponent {}
+export class DetailsComponent implements OnInit {
+  selectedTicket: Observable<Ticket>;
+  id: number;
+
+  constructor(
+    private details: DetailsService,
+    private activatedRoute: ActivatedRoute,
+    private backend: BackendService
+  ) {}
+
+  ngOnInit() {
+    this.id = this.activatedRoute.snapshot.params.id;
+    const preloadedTicket = this.details.load();
+
+    if (preloadedTicket) {
+      this.selectedTicket = preloadedTicket;
+    } else {
+      this.selectedTicket = this.backend.ticket(this.id);
+    }
+  }
+}
